d2c24bd6a4682aab803cf2f12adfe8efc7d361bc
JesusGarciaShakur <jesusrguez970@gmail.com>
2025-01-30 16:32:45 -0600

    02 Header Component and Layout

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,8 +1,7 @@
-export default function HomePage(){
-    return(
-        <div>
-            <h1>Home Page</h1>
-            <p>Esta es la pagina principal</p>
-        </div>
+export default function HomePage() {
+    return (
+        <>
+            <h1>Home</h1>
+        </>
     )
 }
